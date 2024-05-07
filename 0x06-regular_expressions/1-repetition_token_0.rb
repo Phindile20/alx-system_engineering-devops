c2c14c280c6eb8c433adff10dd861a857ff78531
Phindile20 <phindileayandasibiya^@gmail.com>
2024-05-07 06:55:40 -0700

    Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,3 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed to the script
-input = ARGV[0]
-
-# Define the regular expression pattern
-regex = /hbt{2,5}n/  # Matches strings starting with 'hb' followed by 2 to 5 occurrences of 't' and ending with 'n'
-
-# Scan the input for occurrences of the pattern and join them
-matches = input.scan(regex).join
-
-# Output the matched strings
-puts matches
-
+# A regular expression that is matches a given pattern
+puts ARGV[0].scan(/hbt{2,5}n/).join
