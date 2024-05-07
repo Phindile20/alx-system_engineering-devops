b6f6439365daba164b80dd04f08ae618cab266d0
Phindile20 <phindileayandasibiya^@gmail.com>
2024-05-07 06:46:16 -0700

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
-# A regular expression that matches 'School'
-puts ARGV[0].scan(/School/).join
+
+# Get the argument passed to the script
+input = ARGV[0]
+
+# Define the regular expression
+regex = /School/
+
+# Check if the input matches the regular expression
+match = input.match(regex)
+
+# Output the matched string
+puts match[0] if match
+
