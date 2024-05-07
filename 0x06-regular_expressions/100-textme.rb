c091391c9b052ceb7e6f9c8f8a390b005b034198
Phindile20 <phindileayandasibiya^@gmail.com>
2024-05-07 13:16:15 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-def extract_info(log_line)
-  sender = log_line.match(/\[from:(.*?)\]/)[1]
-  receiver = log_line.match(/\[to:(.*?)\]/)[1]
-  flags = log_line.match(/\[flags:(.*?)\]/)[1]
-  
-  "#{sender},#{receiver},#{flags}"
-end
-
-log_file = ARGV[0]
-
-File.open(log_file, "r").each do |line|
-  if line.include?("Sent SMS") || line.include?("Receive SMS")
-    puts extract_info(line)
-  end
-end
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
